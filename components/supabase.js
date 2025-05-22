a5ea15e1e94d301e8d04dfdef8e847b63165c883
Nam Le <104432198+namhoangle236@users.noreply.github.com>
2025-05-22 01:49:38 +0000

    edit eslint rule, still got parsing error from tsx check

--- components/supabase.js
+++ components/supabase.js
@@ -1,5 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl  = 'https://jkpvtzfunmwnhskhhryp.supabase.co'
-const supabaseKey  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprcHZ0emZ1bm13bmhza2hocnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNjQ4MzEsImV4cCI6MjA2Mjg0MDgzMX0.u6yDCLdYMdZLAaFHBU84nsVyne4dYUIjXPRvJisb4aw'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl  = 'https://jkpvtzfunmwnhskhhryp.supabase.co';
+const supabaseKey  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprcHZ0emZ1bm13bmhza2hocnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyNjQ4MzEsImV4cCI6MjA2Mjg0MDgzMX0.u6yDCLdYMdZLAaFHBU84nsVyne4dYUIjXPRvJisb4aw';
+export const supabase = createClient(supabaseUrl, supabaseKey);
